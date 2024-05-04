56d4bc2ee2b38a90f254a7452fef5686992f126d
NOISE <46085246+rcy1314@users.noreply.github.com>
2024-05-04 09:50:10 +0800

    Update getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -1,45 +1,40 @@
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(open('result.html'),"html.parser")
+titleArr = soup.select('.u')
+#标题
+if titleArr:
+    headerTitle = titleArr[0].get_text()
+else:
+    headerTitle = 'Default Header Title'
+formatText = headerTitle + '\n'
 
-try:
-    headerTitle = soup.select('.u')[0].get_text()
-except IndexError:
-    headerTitle = None
-formatText = (headerTitle or '') + '\n'
-
+#新闻
 newsStr = ""
 newsElement = soup.select('.news-wrap > .line')
 for div in newsElement:
-    news = div.get_text() + '\n'
-    newsStr += news
+	news = div.get_text() + '\n'
+	newsStr += news
 formatText += newsStr + '\n'
 
-try:
-    historyTitle = soup.select('.u')[1].get_text()
-except IndexError:
-    historyTitle = None
-formatText += (historyTitle or '') + '\n'
-
+#历史上的今天
+historyTitle = soup.select('.u')[1].get_text()
+formatText += historyTitle + '\n'
+historyArr = soup.select('.history-wrap > .line a')
 index = 0
 history = ''
+
 for a in soup.select('.history-wrap > .line a'):
-    index += 1
-    history += str(index) + '. ' + a.get_text() + '\n'
-formatText += history + '\n'
+	index += 1
+	history += str(index) + '. ' + a.get_text() + '\n'
 
-try:
-    progress = '时间进度条: ' + soup.select('.progress-bar')[0].get_text()
-except IndexError:
-    progress = None
-formatText += (progress or '') + '\n'
+formatText = formatText + history + '\n'
 
-try:
-    progress_text = soup.select('.line')[-1].get_text()
-except IndexError:
-    progress_text = None
-formatText += (progress_text or '') + '\n'
-
+#时间进度条
+progress = '时间进度条: ' + soup.select('.progress-bar')[0].get_text()
+progress_text = soup.select('.line')[-1].get_text()
+formatText += progress + '\n'
+formatText += progress_text + '\n'
 filename = 'result.txt'
-with open(filename, 'w') as file:
-    file.write(formatText)
+with open (filename,'w') as file:
+    file.write(formatText)   
